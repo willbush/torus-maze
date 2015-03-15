f43b575bf2d24d9fcc9e9c2d7cf5af635d35b925
willbush <william.bush@utdallas.edu>
2015-03-15 14:36:10 -0500

    implement printSets; pseudo implement find and union

--- src/WIBUP3.java
+++ src/WIBUP3.java
@@ -1,16 +1,46 @@
 class UnionFind {
+    private int[] sets;
 
     public static UnionFind make(int size) {
         if (size > 0)
             return new UnionFind(size);
         else
-            throw new IllegalArgumentException("size cannot be negative");
+            throw new IllegalArgumentException("size must be greater than zero");
     }
 
     private UnionFind(int size) {
+        sets = new int[size];
 
+        initializeSets();
     }
 
+    private void initializeSets() {
+        for (int i = 0; i < sets.length; i++)
+            sets[i] = -1;
+    }
+
+    public int find(int i) {
+        if (sets[i] < 0)
+            return i;
+        return sets[i];
+    }
+
+    public void union(int x, int y) {
+        if (isOutOfBounds(x, y))
+            throw new IndexOutOfBoundsException("the x or y in union(x, y) is out of bounds");
+
+        sets[y] = x;
+    }
+
+    private boolean isOutOfBounds(int x, int y) {
+        return x > sets.length - 1 || y > sets.length - 1;
+    }
+
+    public void printSets() {
+        for (int value : sets)
+            System.out.print(value + " ");
+        System.out.println();
+    }
 }
 
 public class WIBUP3 {
--- test/unionTest.java
+++ test/unionTest.java
@@ -1,14 +1,55 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-public class unionTest {
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
-    @Test
-    public void canCreateUnionFindObject() {
-        UnionFind u = UnionFind.make(10);
+import static org.junit.Assert.*;
+
+public class unionTest {
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    UnionFind u;
+
+    @Before
+    public void arrange() {
+        u = UnionFind.make(10);
+        System.setOut(new PrintStream(out));
+    }
+
+    @After
+    public void cleanUpOutStream() {
+        System.setOut(null);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void negativeSizeThrowsException() {
-        UnionFind u = UnionFind.make(-1);
+        UnionFind.make(-1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void unionFirstArgOutOfBoundsThrowsException() {
+        u.union(10, 1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void unionSecondArgOutOfBoundsThrowsException() {
+        u.union(1, 10);
+    }
+
+    @Test
+    public void canFindNumber() {
+        assertEquals(2, u.find(2));
+        assertEquals(3, u.find(3));
+        u.union(3, 1);
+        assertEquals(3, u.find(1));
+    }
+
+    @Test
+    public void canPrintSets() {
+        u.printSets();
+        String expected = "-1 -1 -1 -1 -1 -1 -1 -1 -1 -1 \n";
+        assertEquals(expected, out.toString());
     }
 }
